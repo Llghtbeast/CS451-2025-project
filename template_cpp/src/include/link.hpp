2f1aadb844a2698a3bc8db7f52e0ea7a978cdf21
thomas <thomas.pascal.muller@gmail.com>
2025-12-03 11:39:26 +0100

    removed ports from link.cpp and moved all implementations of Link class. Running into some problems in implementation where validity is not respected (some broadcasted messages are never delivered, need to fix

--- template_cpp/src/include/link.hpp
+++ template_cpp/src/include/link.hpp
@@ -20,40 +20,19 @@
 #include "sets.hpp"
 #include "deque.hpp"
 
-/**
- * Derived class representing a sender endpoint of a communication link.
- */
-class Port {
-public:
-/**
- * Constructor to initialize the Port with a socket and its send and receive addresses.
- * @param socket The UDP socket used for communication.
- * @param source_addr The address to which messages will be sent.
- * @param dest_addr The address from which messages will be received.
- */
-  Port(int socket, sockaddr_in source_addr, sockaddr_in dest_addr);
-
-protected:
-  int socket;
-  sockaddr_in source_addr;
-  sockaddr_in dest_addr;
-
-public:
-  static constexpr msg_seq_t window_size = SEND_WINDOW_SIZE; 
-};
 
 /**
- * Derived class representing a sender endpoint of a communication link with message queuing and sending capabilities.
+ * Base class representing the endpoints of a perfect link implementation with send and receive capabilities.
  */
-class SenderPort: public Port {
-public:
-/**
- * Constructor to initialize the SenderPort with a socket and its send and receive addresses.
- * @param socket The UDP socket used for communication.
- * @param source_addr The address to which messages will be sent.
- * @param dest_addr The address from which messages will be received.
- */
-  SenderPort(int socket, sockaddr_in source_addr, sockaddr_in dest_addr);
+class PerfectLink {
+  public:
+  /**
+   * Constructor to initialize the PerfectLink with a socket and its send and receive addresses.
+   * @param socket The UDP socket used for communication.
+   * @param source_addr The address to which messages will be sent.
+   * @param dest_addr The address from which messages will be received.
+   */
+  PerfectLink(int socket, sockaddr_in source_addr, sockaddr_in dest_addr);
   
   /**
    * Enqueues a message to be sent later.
@@ -61,67 +40,34 @@ public:
    * @param m_seq The sequence number of the message.
    */
   void enqueueMessage(proc_id_t origin_id, msg_seq_t m_seq);
-
-/**
- * Send the first enqueued messages.
- * @throws std::runtime_error if sending fails.
- */
+  /**
+  * Send the first enqueued messages.
+  * @throws std::runtime_error if sending fails.
+  */
   void send();
-  
-/** 
- * Receive ACK from receiver and removed corresponding message from queue.
- * @param m_seq The sequence number of the acknowledged message.
-*/
-  void receiveAck(std::vector<std::tuple<msg_seq_t, proc_id_t, msg_seq_t>> acked_messages);
 
-private:
-  msg_seq_t link_seq = 0;
-
-  ConcurrentDeque<std::tuple<msg_seq_t, proc_id_t, msg_seq_t>> message_queue;
-  ConcurrentSet<std::tuple<msg_seq_t, proc_id_t, msg_seq_t>, TupleFirstElementComparator> pending_msgs;
-};
-
-/**
- * Derived class representing a receiver endpoint of a communication link with message receiving and delivering capabilities.
- */
-class ReceiverPort: public Port {
-public:
-/**
- * Constructor to initialize the ReceiverPort with a socket and its send and receive addresses.
- * @param socket The UDP socket used for communication.
- * @param source_addr The address to which messages will be sent.
- * @param dest_addr The address from which messages will be received.
- */
-  ReceiverPort(int socket, sockaddr_in source_addr, sockaddr_in dest_addr);
-  
-/**
- * Add message to delivered list, send ACK to sender, and return true if message was not already delivered. Otherwise, return false.
- * @param message The message to respond to.
- */
-  std::vector<bool> respond(Message message);
-  
-  private:
-  SlidingSet<msg_seq_t> deliveredMessages;
-};
-
-/**
- * Base class representing the endpoints of a perfect link implementation with send and receive capabilities.
- */
-class PerfectLink {
-public:
-/**
- * Constructor to initialize the PerfectLink with a socket and its send and receive addresses.
- * @param socket The UDP socket used for communication.
- * @param source_addr The address to which messages will be sent.
- * @param dest_addr The address from which messages will be received.
- */
-  PerfectLink(int socket, sockaddr_in source_addr, sockaddr_in dest_addr);
-
-  void enqueueMessage(proc_id_t origin_id, msg_seq_t m_seq);
-  void send();
+  /** 
+    * Receive ACK from receiver and removed corresponding message from queue.
+    * @param m_seq The sequence number of the acknowledged message.
+    * Add message to delivered list, send ACK to sender, and return true if message was not already delivered. Otherwise, return false.
+    * @param message The message to respond to.
+    */
   std::vector<bool> receive(Message mes);
 
 private:
-  SenderPort sendPort;
-  ReceiverPort recvPort;
+  int socket;
+  sockaddr_in source_addr;
+  sockaddr_in dest_addr;
+
+  // Sending
+  msg_seq_t link_seq = 0;
+  
+  ConcurrentDeque<std::tuple<msg_seq_t, proc_id_t, msg_seq_t>> message_queue;
+  ConcurrentSet<std::tuple<msg_seq_t, proc_id_t, msg_seq_t>, TupleFirstElementComparator> pending_msgs;
+  
+  // Reception
+  SlidingSet<msg_seq_t> deliveredMessages;
+  
+public:
+  static constexpr msg_seq_t window_size = SEND_WINDOW_SIZE; 
 };
--- template_cpp/src/src/link.cpp
+++ template_cpp/src/src/link.cpp
@@ -1,15 +1,12 @@
 #include "link.hpp"
 
-/* =============================== Port Implementation =============================== */
-Port::Port(int socket, sockaddr_in source_addr, sockaddr_in dest_addr) 
-  : socket(socket), source_addr(source_addr), dest_addr(dest_addr) {}
-
-SenderPort::SenderPort(int socket, sockaddr_in source_addr, sockaddr_in dest_addr)
-  : Port(socket, source_addr, dest_addr), message_queue(), pending_msgs(true)
+PerfectLink::PerfectLink(int socket, sockaddr_in source_addr, sockaddr_in dest_addr)
+  : socket(socket), source_addr(source_addr), dest_addr(dest_addr), 
+    message_queue(), pending_msgs(true), deliveredMessages()
 {}
 
-void SenderPort::enqueueMessage(proc_id_t origin_id, msg_seq_t m_seq)
-{  
+void PerfectLink::enqueueMessage(proc_id_t origin_id, msg_seq_t m_seq)
+{
   // Create message
   link_seq++;
   std::tuple<msg_seq_t, proc_id_t, msg_seq_t> messageTuple = std::make_tuple(link_seq, origin_id, m_seq);
@@ -18,7 +15,7 @@ void SenderPort::enqueueMessage(proc_id_t origin_id, msg_seq_t m_seq)
   message_queue.push_back(messageTuple); 
 }
 
-void SenderPort::send()
+void PerfectLink::send()
 {
   // No messages to send
   if (pending_msgs.empty() && message_queue.empty()) return;
@@ -61,66 +58,30 @@ void SenderPort::send()
   }
 }
 
-void SenderPort::receiveAck(std::vector<std::tuple<msg_seq_t, proc_id_t, msg_seq_t>> acked_messages)
-{
-  // Remove messages acknowledged by receiver
-  pending_msgs.erase(acked_messages);
-}
-
-ReceiverPort::ReceiverPort(int socket, sockaddr_in source_addr, sockaddr_in dest_addr)
-  : Port(socket, source_addr, dest_addr), deliveredMessages() {}
-
-std::vector<bool> ReceiverPort::respond(Message message)
-{
-  // deliveredMessages.display();
-
-  // Update delivered message set and construct delivery status vector
-  std::vector<bool> delivery_status = deliveredMessages.insert(message.getSeqs());
-
-  // Transform message to ack and serialize
-  Message ackMsg = message.toAck();
-
-  // Send ACK to sender
-  if (sendto(socket, ackMsg.serialize(), ackMsg.serializedSize(), 0, reinterpret_cast<const sockaddr *>(&dest_addr), sizeof(dest_addr)) < 0) {
-    std::ostringstream os;
-    os << "Failed to send ACK (errno: " << strerror(errno) << ") from " << source_addr.sin_addr.s_addr << ":" << source_addr.sin_port << " to " << dest_addr.sin_addr.s_addr << ":" << dest_addr.sin_port;
-    std::cout << os.str() << "\n";
-    return {};
-    // throw std::runtime_error(os.str());
-  }
-  return delivery_status;
-}
-
-/* =============================== Link Implementation =============================== */
-PerfectLink::PerfectLink(int socket, sockaddr_in source_addr, sockaddr_in dest_addr)
-  : sendPort(socket, source_addr, dest_addr), recvPort(socket, source_addr, dest_addr) {}
-
-void PerfectLink::enqueueMessage(proc_id_t origin_id, msg_seq_t m_seq)
-{
-  sendPort.enqueueMessage(origin_id, m_seq);
-}
-
-void PerfectLink::send()
-{
-  return sendPort.send();
-}
-
 std::vector<bool> PerfectLink::receive(Message mes)
 {
   MessageType type = mes.getType();
 
   if (mes.getType() == MES) { 
-    // Deliver message to receiver link
-    return recvPort.respond(mes);
+    // Update delivered message set and construct delivery status vector
+    std::vector<bool> delivery_status = deliveredMessages.insert(mes.getSeqs());
+
+    // Transform message to ack and serialize
+    Message ackMsg = mes.toAck();
+
+    // Send ACK to sender
+    if (sendto(socket, ackMsg.serialize(), ackMsg.serializedSize(), 0, reinterpret_cast<const sockaddr *>(&dest_addr), sizeof(dest_addr)) < 0) {
+      std::ostringstream os;
+      os << "Failed to send ACK (errno: " << strerror(errno) << ") from " << source_addr.sin_addr.s_addr << ":" << source_addr.sin_port << " to " << dest_addr.sin_addr.s_addr << ":" << dest_addr.sin_port;
+      std::cout << os.str() << "\n";
+      return {};
+      // throw std::runtime_error(os.str());
+    }
+    return delivery_status;
   }
   else if (type == ACK) {
-    // Process ACK on sender link
-    sendPort.receiveAck(mes.getPayloads());
-    // std::cout << "Processed ACK for messages: ";
-    for (msg_seq_t seq: mes.getSeqs()) {
-      // std::cout << seq << " ";
-      }
-    // std::cout << "" << std::endl;
+    // Remove messages acknowledged by receiver
+    pending_msgs.erase(mes.getPayloads());
     return {};
   }
   else {
